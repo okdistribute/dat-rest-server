a619ae82f3a4c1dff4fb53b335c60ead3ac2cf34
Karissa McKelvey <krmckelv@gmail.com>
2015-09-15 15:46:06 -0700

    fix bug in cli

--- cli.js
+++ cli.js
@@ -2,7 +2,9 @@
 var args = require('minimist')(process.argv.slice(2))
 var server = require('./')
 
-server(args)
+server(args, function (server, port) {
+  server.listen(port)
+})
 
 function usage () {
   console.log('dat-rest-server --port=<port-num> --path=<path/to/dat>')
